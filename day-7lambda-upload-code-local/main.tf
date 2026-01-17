a25119530db66eefc50881d99aa0348253140fb6
raj <raj6305@gmail.com>
2026-01-17 15:59:41 +0530

    day-7completed

---
+++ day-7lambda-upload-code-local/main.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "test_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+
+
+}
+
+resource "aws_iam_role_policy_attachment" "name" {
+    role = aws_iam_role.test_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  
+}
+
+resource "aws_lambda_function" "name" {
+    function_name = "lambda_function"
+    role = aws_iam_role.test_role.arn
+    handler = "lambda_function.lambda_handler"
+    runtime = "python3.12"
+    timeout = 900
+    memory_size = 128
+    filename = "lambda_function.zip"
+
+    source_code_hash = filebase64sha256("lambda_function.zip")
+  
+}
+
